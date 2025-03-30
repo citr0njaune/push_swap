829d407778a4618a6b23c5a866cb08501d38f76d
charline phan <you@example.com>
2025-03-31 00:53:33 +0200

    cha

---
+++ op_rotate.c
@@ -0,0 +1,33 @@
+#include "push_swap.h"
+
+void	rotate(t_node **stack)
+{
+    t_stack *temp;
+    t_stack *current;
+
+    temp = *stack;
+    *stack = *stack->next;
+    current = *stack;
+    while (current->next)
+        current = current->next
+    current->next = temp;
+    temp->next = 0;
+}
+
+void	ra(t_node **a)
+{
+	rotate(a);
+	ft_putendl_fd("ra", 1);
+}
+
+void	rb(t_node **b)
+{
+	rotate(b);
+	ft_putendl_fd("rb", 1);
+}
+void	rr(t_node **a, t_node **b)
+{
+	rotate(a);
+	rotate(b);
+	ft_putendl_fd("rr", 1);
+}
