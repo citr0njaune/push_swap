0f7b81d084191c387020778c5439f8682df20ec6
chphan <charlinephan20@gmail.com>
2025-03-31 20:34:21 +0200

    op_push.c

---
+++ op_push.c
@@ -0,0 +1,29 @@
+#include "push_swap.h"
+
+void    push(t_stack **stack, t_stack **stack2)
+{
+    t_stack *temp;
+    t_stack *temp2;
+
+    temp = malloc(sizeof(t_stack));
+    if (!temp)
+        return ;
+    temp->value = *stack2->value;
+    temp->next = *stack;
+    *stack = temp;
+    temp2 = *stack2;
+    *stack2 = *stack->next;
+    ft_free(temp);
+}
+
+void    pa(t_stack **a, t_stack **b)
+{
+    push(a, b);
+	ft_putendl_fd("pa", 1);
+}
+
+void    pb(t_stack **b, t_stack **a)
+{
+    push(b, a);
+	ft_putendl_fd("pb", 1);
+}
